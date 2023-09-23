0f4ca8c14c42951023d77a96994dca9b3cc309fe
RafayMughal_70 <Rafaymughal661@gmail.com>
2023-09-23 20:10:43 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,45 @@
+import inquirer from "inquirer";
+// Calculator Program:
+let questions = [{
+        name: ['num1'],
+        type: 'number',
+        message: 'Enter your 1st number'
+    },
+    {
+        name: ["num2"],
+        type: "number",
+        message: ["Enter your 2nd number:"]
+    },
+    {
+        name: 'operation',
+        type: 'list',
+        choices: ["+", "*", "-", "/", '**', "%"]
+    }];
+let Result = await inquirer.prompt(questions);
+try {
+    switch (Result.operation) {
+        case "+":
+            console.log(`${Result.num1}+${Result.num2} == ${Result.num1 + Result.num2} `);
+            break;
+        case "-":
+            console.log(`${Result.num1}-${Result.num2} == ${Result.num1 - Result.num2} `);
+            break;
+        case "*":
+            console.log(`${Result.num1}*${Result.num2} == ${Result.num1 * Result.num2} `);
+            break;
+        case "/":
+            console.log(`${Result.num1}/${Result.num2} == ${Result.num1 / Result.num2} `);
+            break;
+        case "**":
+            console.log(`${Result.num1}**${Result.num2}==${Result.num1 ** Result.num2}`);
+            break;
+        case "%":
+            console.log(`${Result.num1}%${Result.num2}==${Result.num1 % Result.num2}`);
+        default:
+            break;
+    }
+}
+catch (error) {
+    console.log("An error occurred:", error);
+}
+;
